6dfe79f21dcf13eb9381891c7abfff3a1afcbf42
Syedsafeeruddin <syedsafeeruddi439@gmail.com>
2024-06-18 17:00:00 +0500

    lecture01-variables

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log("zli");
+const id = 8923;
+let name = 'safeer';
+var age = 'twenty';
+address = 'karachi'; // not recommended method!!!
+let country
+ 
+/*
+prefer not use var bcz of scope & always use let or const when declaring a variable
+*/
+
+console.log(id);
+console.table([name, age,address,country]);
