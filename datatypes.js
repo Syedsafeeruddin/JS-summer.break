c51c68943dcf977ee0a82072345ece9ffa498753
Syedsafeeruddin <syedsafeeruddi439@gmail.com>
2024-06-21 21:27:37 +0500

    variables lec 02

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+let name = 'safeer';
+let age = '20';
+let IsOnline = false;
+
+// PRIMITIVE DATATYPES:
+// 1. Number
+// 2. String => ""
+// 3. Boolean => true/false
+// 4. BigInt (for bigger numbers)
+// 5. Null => Standalone value
+//    let age = null; --- this has a null value
+// 6. Undefined => haven't decided a value
+//    let age = ; --- the value has not been assigned yet
+// 7. Symbol => unique 
+// 8. Object --- will study later
+
+// (typeof) => to determine the data type of any variable 
