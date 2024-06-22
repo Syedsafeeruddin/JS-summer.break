8f0004ca78612a448cbfab1df9f6c9aedb1101b0
Syedsafeeruddin <syedsafeeruddi439@gmail.com>
2024-06-22 22:36:31 +0500

    lecture 04 - Comparisons

---
+++ comparison.js
@@ -0,0 +1,24 @@
+// == => this operator checks values
+// console.log(5 == "5");
+
+// === => this operator strictly checks values as in both should have a same data type
+// console.log("5" === "5");
+
+// Increment and decrement
+let a = 5;
+
+// console.log(a++);  postfix
+// console.log(a--);  postfix
+// console.log(++a);  prefix
+// console.log(--a);  prefix
+
+// Comparison Operator
+// =
+
+// Arithmetic Operator
+console.log(10 + 20);
+console.log(10 - 20);
+console.log(10 * 20);
+console.log(10 / 20);
+console.log(10 % 20);
+console.log(10 ** 20);
