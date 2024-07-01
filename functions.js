29446c3ad5491b5e5449c58884f32a6f87734d24
Syedsafeeruddin <syedsafeeruddi439@gmail.com>
2024-07-01 14:44:18 +0500

    Funtions

---
+++ functions.js
@@ -0,0 +1,59 @@
+// function myFunction() {
+//     console.log('s');
+//     console.log('a');
+//     console.log('f');
+//     console.log('e');
+//     console.log('e');
+//     console.log('r');
+// }
+// myFunction ---> this is Reference
+// myFunction() ---> this is execution\
+
+// function Addition(num1, num2) {
+//     let result = num1 + num2;
+//     console.log(`Result is ${result}`);
+//     return result
+// }
+
+// Addition(2,9)
+
+// function LogIn(user) {
+//     if (!user) {
+//         console.log("Please enter a username");
+//     }
+//      console.log(`${user} just logged in`);
+//      return
+// }
+// LogIn('safeer');
+
+// function addItems(params) {
+    
+// }
+
+// function myFunction(){
+//     console.log(this);
+// }
+
+// myFunction();
+
+// const myFunction = function(){
+//     console.log('a function');
+// }
+// myFunction();
+
+// this is a simple function
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+// console.log(add(2,3));
+
+
+// // this is an arrow function
+// const ADD = (num1, num2) => {
+//     return num1 + num2  
+// }
+// console.log(ADD(4,5));
+
+// const addTwo = (num1, num2) => console.log((num1 + num2)) // implicit return, here we can't use return keyword
+
+// addTwo(4,4)
