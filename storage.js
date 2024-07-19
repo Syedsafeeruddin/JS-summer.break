1425986f730e07b66ac13f20df87a6c331c3e236
Syedsafeeruddin <syedsafeeruddi439@gmail.com>
2024-07-19 15:21:53 +0500

    Storage Js

---
+++ storage.js
@@ -0,0 +1,7 @@
+// localStorage.setItem(key, value); ---> syntax, here key is like a variable and value is its value
+// localStorage.setItem("lastname", "Smith"); ---> to save data
+// localStorage.getItem("lastname"); ---> to read data
+// localStorage.removeItem(key); ---> to remove data
+// localStorage.clear(); ---> to delete data
+
+// sessionStorage when the browser is closed it is deleted localStorage is not deleted what so ever
